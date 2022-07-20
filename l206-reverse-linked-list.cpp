fddd75301544760c95dd1ea808f6817c7442afac
Link Wu <coin12.wu@gmail.com>
2022-07-19 23:23:10 -0400

    Update l206-reverse-linked-list.cpp

--- l206-reverse-linked-list.cpp
+++ l206-reverse-linked-list.cpp
@@ -1,11 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         if(!head){ return head; }
+//         ListNode* last = head;
+//         ListNode* current = last->next;
+//         ListNode* next;
+//         head->next = nullptr;
+//         while(current){
+//             next = current->next;
+//             current->next = last;
+//             last = current;
+//             current = next;
+//         }
+//         return last;
+//     }
+// };
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if(!head){ return head; }
-        ListNode* last = head;
-        ListNode* current = last->next;
+        ListNode* last = nullptr;
+        ListNode* current = head;
         ListNode* next;
-        head->next = nullptr;
         while(current){
             next = current->next;
             current->next = last;
