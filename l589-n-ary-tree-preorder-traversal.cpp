1b8d5b4e15a52ac8af97fb6c26cf6d866d207adf
tccoin <junzhewu@umich.edu>
2022-08-13 22:50:55 -0400

    add l589

--- l589-n-ary-tree-preorder-traversal.cpp
+++ l589-n-ary-tree-preorder-traversal.cpp
@@ -45,23 +45,25 @@ public:
 class Solution
 {
 public:
-    vector<int> preorder(Node *root)
+    vector<int> ans;
+    vector<int> postorder(Node *root)
     {
-        vector<int> ans;
         stack<Node *> todo;
-        if (root)
-            todo.push(root);
+        if (!root)
+            return ans;
+        todo.push(root);
         Node *current;
         while (!todo.empty())
         {
             current = todo.top();
             todo.pop();
             ans.push_back(current->val);
-            for (auto it = current->children.rbegin(); it != current->children.rend(); it++)
+            for (auto child : current->children)
             {
-                todo.push(*it);
+                todo.push(child);
             }
         }
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
