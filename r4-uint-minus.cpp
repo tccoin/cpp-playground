e349e0504caa9b8c54540ef80e56d30ddc910bb7
tccoin <junzhewu@umich.edu>
2022-09-01 01:11:45 -0400

    test uint minus operation

---
+++ r4-uint-minus.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+  uint64_t a = 100;
+  uint64_t b = 99;
+  uint64_t c = 0.01 * 1e9;
+  cout << b - a << endl << c << endl;
+  return 0;
+}
