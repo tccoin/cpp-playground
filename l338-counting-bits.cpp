1079c77d23196fc26d651522ee4c6c914352864c
tccoin <junzhewu@umich.edu>
2022-10-08 00:53:48 -0400

    update l338-counting-bits

---
+++ l338-counting-bits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+ public:
+  vector<int> countBits(int n) {
+    vector<int> dp(n + 1, 0);
+    for (int i = 0; i < n + 1; i++) {
+      dp[i] = dp[i / 2] + (i & 1);
+    }
+    return dp;
+  }
+};
